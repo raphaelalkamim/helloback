df2122a77108992b72363883969edbcf9c26ca13
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2021-12-10 14:55:48 -0300

    final changes dto

--- src/main/java/br/hello/helloback/entity/AccessKey.java
+++ src/main/java/br/hello/helloback/entity/AccessKey.java
@@ -11,7 +11,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
