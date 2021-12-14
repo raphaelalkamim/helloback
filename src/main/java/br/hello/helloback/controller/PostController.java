95bacf878fdbc4009db2d1bf721d1bc816d2c716
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2021-12-14 16:22:06 -0300

    error utf8 try fix

--- src/main/java/br/hello/helloback/controller/PostController.java
+++ src/main/java/br/hello/helloback/controller/PostController.java
@@ -13,6 +13,7 @@ import br.hello.helloback.entity.Widget;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -118,7 +119,8 @@ public class PostController {
 
     // POST
 
-    @RequestMapping(value = "/channels/{channelId}/users/{userID}/posts", method = RequestMethod.POST)
+    @RequestMapping(value = "/channels/{channelId}/users/{userID}/posts", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE},
+    produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<PostDTO> createPost(@Valid @RequestBody Post post,
             @PathVariable(value = "channelId") Long channelId,
             @PathVariable(value = "userID") Long userId) {
